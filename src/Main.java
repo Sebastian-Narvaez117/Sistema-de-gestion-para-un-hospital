6fdc303967cc9e359fd6959de2416eaac9264b04
Sebastian <joseph.narvaez@unl.edu.ec>
2024-05-21 12:41:46 -0500

    mejor1

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,8 @@ public class Main {
             System.out.println(expedienteMedico.getTratamiento());
             System.out.println(expedienteMedico.getPrescripcion());
 
+            //hola
+
 
     }
 }
